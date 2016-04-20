ae78561337bbd42f791731847c8954546f9d40cb
duthientandeveloper <dttdeveloper2402@gmail.com>
2016-04-20 18:59:31 +0700

    fix duplicate station

--- app_api/controllers/graphCtrl.js
+++ app_api/controllers/graphCtrl.js
@@ -9,21 +9,23 @@ module.exports.init = function(req,res,next){
     if (err) throw err;
     arrIdStaion.sort(sortNumber);
     arrIdStaion.forEach(function(idStation) {
-      Station.find({id:idStation},function(err,arrDupStation) {
-          var distance =[],belongRoute=[],nextStation=[];
-          arrDupStation.forEach(function(dupSation) {
-            distance.push(dupSation.distance);
-            belongRoute.push(dupSation.idRoute);
-            nextStation.push(dupSation.nextId);
-          });
-          var newGraph = Graph({
-            id:idStation,
-            distance:distance,
-            belongRoute:belongRoute,
-            nextStation:nextStation
-          });
-          newGraph.save(function () {});
-      });
+        Station.find({id:idStation},function(err,arrDupStation) {
+            var distance =[],belongRoute=[],nextStation=[];
+            arrDupStation.forEach(function(dupSation) {
+              if(nextStation.indexOf(dupSation.nextId)===-1){
+                nextStation.push(dupSation.nextId);
+                distance.push(dupSation.distance);
+                belongRoute.push(dupSation.idRoute);
+              }
+            });
+            var newGraph = Graph({
+              id:idStation,
+              distance:distance,
+              belongRoute:belongRoute,
+              nextStation:nextStation
+            });
+            newGraph.save(function () {});
+        });
     });
     res.send('Xong');
   });
